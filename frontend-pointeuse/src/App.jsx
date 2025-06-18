f7459bb71d056dfa7a36321eeadb69cb4166a128
jbuguys <wassimnefzi120@gmail.com>
2025-06-18 19:18:54 +0100

    fix

--- frontend-pointeuse/src/App.jsx
+++ frontend-pointeuse/src/App.jsx
@@ -4,12 +4,12 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
     return (
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="employe" element={<Employe />} />
-                </Route>
-            </Routes>
+        <Routes basename="/projet_pointeuse">
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="employe" element={<Employe />} />
+            </Route>
+        </Routes>
     );
 }
 
