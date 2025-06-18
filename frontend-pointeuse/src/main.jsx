5615d37a0054343d000075f713f3464e5e7cd06e
jbuguys <wassimnefzi120@gmail.com>
2025-06-18 20:39:31 +0100

    test

--- frontend-pointeuse/src/main.jsx
+++ frontend-pointeuse/src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter  basename='/projet_pointeuse'>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
