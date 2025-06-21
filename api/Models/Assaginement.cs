efe03be86392cd883a8c3a109966f851f71ab322
jbuguys <wassimnefzi120@gmail.com>
2025-06-21 14:28:25 +0100

    added assaginement

---
+++ api/Models/Assaginement.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Assaginement
+    {
+        public int Id { get; set; }
+        public int? ScheduleId { get; set; }
+        public Schedule? Schedule { get; set; }
+        public int? ServiceId { get; set; }
+        public Service? Service { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
