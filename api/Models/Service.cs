3f8e30ebfbd927e7536a2a48c45c7714f64094dd
jbuguys <wassimnefzi120@gmail.com>
2025-06-21 13:34:47 +0100

    added Service

---
+++ api/Models/Service.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Active { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TolereanceCalcul { get; set; }
+        public bool SoumisHS { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DureeLimite { get; set; }
+    }
+}
