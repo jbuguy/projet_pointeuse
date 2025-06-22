af26b5c879f4bd99164f068ce2428e30bdb5d273
jbuguys <wassimnefzi120@gmail.com>
2025-06-22 22:20:14 +0100

    changed table style and added suivi heure

--- frontend-pointeuse/src/App.jsx
+++ frontend-pointeuse/src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./components/Layout";
 import Employe from "./pages/Employe";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import SuiviHeure from "./pages/SuiviHeure";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="employe" element={<Employe />} />
+                <Route path="suivi_heure" element={<SuiviHeure />} />
             </Route>
         </Routes>
     );
